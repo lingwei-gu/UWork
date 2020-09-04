f694ccd4b17f8e73ae36e9af6f2424f61d75e547
Iris Mu <5215lingyim@gmail.com>
2020-09-04 16:06:40 -0400

    ...

--- scrape_data/secret.py
+++ scrape_data/secret.py
@@ -1,2 +1,11 @@
-account = ""
-password = ""
+import zlib
+from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
+
+def foobar(obscured:bytes) -> bytes:
+    return zlib.decompress(b64d(obscured))
+
+foo = b'eNrLMc0tBQADogGE'
+bar = b'eNrzTsxJzUzJL07OL0g0MjY2VgQAOW0FvA=='
+
+account = foobar(foo).decode()
+password = foobar(bar).decode()
